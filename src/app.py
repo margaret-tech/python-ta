f747ce4e821507b1a63bdcfdc10aac2c28f420d7
margaret-tech <maggie.learn@gmail.com>
2021-02-06 03:47:17 +0300

    My SCA Mentee Python Assessment

--- src/app.py
+++ src/app.py
@@ -4,6 +4,26 @@ import os
 
 def palindrome(s):
     # your code goes here
+    print("Word to test: ", s)
+    # remove spaces
+    word_no_space = s.replace(" ", "")
+    # convert to uppercase
+    new_word = word_no_space.upper()
+    print("New is: ", new_word)
+    # convert word to list of characters
+    word_list = list(new_word)
+    print("New as list: ", word_list)
+    # create a copy of word list
+    reverse_word_list = word_list.copy()
+    # reverse the characters in the word list
+    reverse_word_list.reverse()
+    print("Reverse as list: ", reverse_word_list)
+    # compare reversed list and original list
+    if word_list == reverse_word_list:
+        return True
+    else:
+        return False
+
 
 def solution(s):
     return palindrome(s)
